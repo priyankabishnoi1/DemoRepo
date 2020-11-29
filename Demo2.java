721098397af43245e49c24aa788cc00a1f59079a
priyankabishnoi1 <75195462+priyankabishnoi1@users.noreply.github.com>
2020-11-28 22:08:56 -0800

    Update Demo2.java

--- Demo2.java
+++ Demo2.java
@@ -3,5 +3,6 @@ class Demo2
 	public static void main(String args[]){
 		System.out.println("Print this info");
 		System.out.println("Printing second line");
+		System.out.println("Printing third line");
 	}
 }
